8751ab9327b7964af84ff3905cc7798934103e1c
Baaka Tsutskhvashvili <tsutskhvashvili.baaka@gmail.com>
2025-10-07 21:47:05 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Replace with your repository name if different
+    plugins: [react()],
+    base: './',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 })
